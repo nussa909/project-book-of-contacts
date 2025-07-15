ba15dd5ea0672b7ac0b6468cc13afb509da597b8
Anna Molodchenkova <anna.molodchenkova@gmail.com>
2025-07-15 16:02:50 +0300

    add_note command

--- src/notebook.py
+++ src/notebook.py
@@ -1,41 +1,72 @@
 
 from collections import UserList
 
+
 class Note:
-    def __init__(self, header:str, text:str, tags = {}):
+    current_id = 0
+
+    def __init__(self, header: str, text: str, tags={}):
+        self.id = Note.__get_next_id()
         self.header = header
         self.text = text
-        self.tags = tags
-
-    def is_tag_added(self, tag):
-        return tag in self.tags
+        self.tags = set(tags)
 
     def add_tag(self, tag):
-        if not self.is_tag_added(tag):
-            self.tags.append(tag)
+        if not tag in self.tags:
+            self.tags.add(tag)
 
     def remove_tag(self, tag):
-        if self.is_tag_added(tag):
+        if tag in self.tags:
             self.tags.remove(tag)
 
+    @classmethod
+    def __get_next_id(cls):
+        cls.current_id += 1
+        return cls.current_id
+
     def __str__(self):
-        return f"{self.header}\ntags:{self.tags}\n{self.text}"
-    
+        return f"#{self.id}:{self.header}\ntags:{list(self.tags)}\n{self.text}"
+
     def __repr__(self):
-        return f"{self.header}"
+        return f"#{self.id}:{self.header}"
 
 
 class Notebook(UserList):
 
-    def add_note(self, note:Note):
-        pass        
+    def add_note(self, note: Note):
+        self.data.append(note)
 
-    def edit_note(self, note:Note):
+    def remove_note(self, id: int):
+        note = self.find_note_by_id(id)
+        if note:
+            self.data.remove(note)
+
+    def find_note_by_tags(self, tags: str):
         pass
 
-    def remove_note(self, note:Note):
+    def find_note_by_id(self, id: int):
+        for note in self.data:
+            if note.id == id:
+                return note
+
+    def show(self):
         pass
 
-    def find_note_by_tags(self, tags:str):
-        pass
-    
+    def __str__(self):
+        return f"Notebook:\n{"\n".join(str(note) for note in self.data)}"
+
+# note = Note("Покупки", "Купити молоко та яйця",{"important"})
+# note.add_tag("buy")
+# print(note)
+
+# # note.remove_tag("important")
+# print(note)
+
+# note2 = Note("email password", "email: my_email@gmail.com, pass:1234")
+# note2.add_tag("important")
+
+# notebook = Notebook()
+# notebook.add_note(note)
+# notebook.add_note(note2)
+
+# print(notebook)
